0c742d25f9be328f9300c09cb67d40c2a95e7a7b
Pedro Leão Nogueira Steinbach <94500870+RRonanS@users.noreply.github.com>
2023-12-06 20:46:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ if __name__ == '__main__':
     try:
         # from codigos.outros import write_gerador
         import game
-        game.run()
-        """from codigos.interfaces import main_menu
-        main_menu.main_menu()"""
+        # game.run()
+        from codigos.interfaces import main_menu
+        main_menu.main_menu()
     except KeyboardInterrupt:
         print('Jogo encerrado pelo console')
-    """except Exception as e:
-        print(f'Ocorreu um erro:\n {str(e)}')"""
+    except Exception as e:
+        print(f'Ocorreu um erro:\n {str(e)}')
